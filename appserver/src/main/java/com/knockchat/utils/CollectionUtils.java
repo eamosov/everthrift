68acdb9c79e9e0101c42fac40540b96881c653fd
Evgeniy Amosov <efreet@efreet.ru>
2015-09-29 13:00:55 +0300

    no message

--- appserver/src/main/java/com/knockchat/utils/CollectionUtils.java
+++ appserver/src/main/java/com/knockchat/utils/CollectionUtils.java
@@ -8,6 +8,7 @@ import gnu.trove.procedure.TLongProcedure;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
@@ -230,5 +231,109 @@ public class CollectionUtils {
 				return;
 			}
 		}
+	}
+	
+	static class JoinedCollectionView<E> implements Collection<E> {
+
+		private final Collection<? extends E>[] items;
+
+		public JoinedCollectionView(final Collection<? extends E>[] items) {
+			this.items = items;
+		}
+
+		@Override
+		public boolean addAll(final Collection<? extends E> c) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void clear() {
+			for (final Collection<? extends E> coll : items) {
+				coll.clear();
+			}
+		}
+
+		@Override
+		public boolean contains(final Object o) {
+			for (final Collection<? extends E> coll : items) {
+				if (coll.contains(o))
+					return true;
+			}
+			return false;
+		}
+
+		@Override
+		public boolean containsAll(final Collection<?> c) {
+			for (Object o:c){
+				if (!contains(o))
+					return false;
+			}
+			
+			return true;			
+		}
+
+		@Override
+		public boolean isEmpty() {
+			return !iterator().hasNext();
+		}
+
+		@Override
+		public Iterator<E> iterator() {
+			return Iterables.concat(items).iterator();
+		}
+
+		@Override
+		public boolean remove(final Object o) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean removeAll(final Collection<?> c) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean retainAll(final Collection<?> c) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public int size() {
+			int ct = 0;
+			for (final Collection<? extends E> coll : items) {
+				ct += coll.size();
+			}
+			return ct;
+		}
+
+		@Override
+		public Object[] toArray() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public <T> T[] toArray(T[] a) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean add(E e) {
+			throw new UnsupportedOperationException();
+		}
+
+	}
+
+	/**
+	 * Returns a live aggregated collection view of the collections passed in.
+	 * <p>
+	 * All methods except {@link Collection#size()}, {@link Collection#clear()},
+	 * {@link Collection#isEmpty()} and {@link Iterable#iterator()} throw
+	 * {@link UnsupportedOperationException} in the returned Collection.
+	 * <p>
+	 * None of the above methods is thread safe (nor would there be an easy way
+	 * of making them).
+	 */
+	public static <T> Collection<T> combine(final Collection<? extends T>... items) {
+		return new JoinedCollectionView<T>(items);
 	}
 }
