16c4a8b2837fe2ec43420307d817de98403d773a
Evgeniy Amosov <efreet@efreet.ru>
2016-03-15 10:23:05 +0300

    remove tmc

--- tmc/src/main/resources/management-console/bin/keychain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export main_class="com.terracotta.management.cli.keychain.KeyChainCli"
-export cli_name="Keychain Client"
-
-unset CDPATH
-root=`dirname $0`
-
-$root/management-cli-common.sh -S  "$@"
--- tmc/src/main/resources/management-console/bin/usermanagement.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export main_class="com.terracotta.management.cli.auth.UserManagementCli"
-export cli_name="User Management Client"
-unset CDPATH
-root=`dirname $0`
-
-$root/management-cli-common.sh "$@"
