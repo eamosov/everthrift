6c2ac165c551d18d52f3810d6635c73c49e3ecab
Evgeniy Amosov <efreet@efreet.ru>
2016-03-30 20:17:33 +0300

    no message

--- appserver/src/main/java/com/knockchat/appserver/AppserverApplication.java
+++ appserver/src/main/java/com/knockchat/appserver/AppserverApplication.java
@@ -24,7 +24,6 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
