5e1b9171df6b110ee5d7024d17ed5c6a8d5d234a
Evgeniy Amosov <efreet@efreet.ru>
2017-07-08 10:56:13 +0300

    ThriftFormatter

--- appserver/src/main/java/org/everthrift/appserver/utils/thrift/ThriftFormatter.java
+++ appserver/src/main/java/org/everthrift/appserver/utils/thrift/ThriftFormatter.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Multimaps;
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Shorts;
 import org.apache.thrift.TBase;
+import org.apache.thrift.TDoc;
 import org.apache.thrift.TEnum;
 import org.apache.thrift.TFieldIdEnum;
 import org.apache.thrift.TFieldRequirementType;
@@ -19,7 +20,9 @@ import org.apache.thrift.meta_data.StructMetaData;
 import org.apache.thrift.protocol.TType;
 import org.everthrift.appserver.controller.ThriftControllerInfo;
 import org.everthrift.utils.Pair;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Method;
 import java.net.URLEncoder;
 import java.util.Collection;
 import java.util.Collections;
@@ -27,6 +30,9 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class ThriftFormatter {
@@ -52,6 +58,8 @@ public class ThriftFormatter {
         Multimap<String, ThriftControllerInfo> mm = Multimaps.index(cInfo, ThriftControllerInfo::getServiceName);
 
         final StringBuilder sb = new StringBuilder();
+        sb.append(head());
+
         for (Map.Entry<String, Collection<ThriftControllerInfo>> e : mm.asMap().entrySet()) {
             sb.append(formatService(e.getKey(), e.getValue()));
         }
@@ -61,16 +69,40 @@ public class ThriftFormatter {
     public String formatService(String serviceName, Collection<ThriftControllerInfo> cInfos) {
         final StringBuilder sb = new StringBuilder();
 
-        sb.append("<div class=\"service\">\n");
-        sb.append(String.format("<span>%s{</span>\n", serviceName));
-        sb.append("<ol>\n");
+        sb.append("<div class=\"service\" style=\"padding-bottom:10px;\">\n");
+        sb.append(String.format("<span><span class=\"pl-k\">service</span> <span class=\"pl-en\">%s</span> {</span>\n", serviceName));
+        sb.append("<div class=\"methods\" style=\"padding: 5px 0px 0px 20px;\">\n");
         for (ThriftControllerInfo i : cInfos) {
-            sb.append(String.format("\t<li style=\"list-style-type: none;\">%s</li>\n", formatMethod(i)));
+            sb.append("<div class=\"method\" style=\"padding-bottom: 5px;\">\n");
+            sb.append("<div class=\"pl-c\">" + getTDocMethodComment(i.getArgCls()) + "</div>\n");
+            sb.append(formatMethod(i.getMethodName(), i.getArgCls(), i.getResultCls()));
+            sb.append("</div>");
         }
-        sb.append("</ol>\n");
+        sb.append("</div>\n");
         sb.append("<span>}</span>\n");
-        sb.append("<div/>\n");
+        sb.append("</div>\n");
         return sb.toString();
+    }
+
+    public String getTDocMethodComment(Class methodArgCls) {
+        final Pattern pattern = Pattern.compile("(^.+)\\.([^\\.]+)_args$");
+        final Matcher matcher = pattern.matcher(methodArgCls.getCanonicalName());
+
+        if (matcher.matches()) {
+            final Class iface;
+            try {
+                iface = Class.forName(matcher.group(1) + "$Iface");
+            } catch (ClassNotFoundException e) {
+                return "";
+            }
+            final String methodName = matcher.group(2);
+            for (Method m : iface.getMethods()) {
+                if (m.getName().equals(methodName)) {
+                    return Optional.ofNullable(m.getAnnotation(TDoc.class)).map(TDoc::value).orElse("");
+                }
+            }
+        }
+        return "";
     }
 
     public String formatMethod(String name, Class<? extends TBase> argsCls, Class<? extends TBase> resultCls) {
@@ -89,7 +121,7 @@ public class ThriftFormatter {
         }
 
         final StringBuilder sb = new StringBuilder();
-        sb.append("<span class=\"method\">" + formatTypeName(result) + " " + name + "(");
+        sb.append("<span class=\"pl-k\">" + formatTypeName(result, false) + "</span> " + "<span class=\"pl-en\">" + name + "</span>" + "(");
 
         final Pair<Class<? extends TBase>, Map<? extends TFieldIdEnum, FieldMetaData>> argsMap = ThriftUtils.getRootThriftClass(argsCls);
         final List<TFieldIdEnum> argsFields = sortedKeys(argsMap.second);
@@ -100,7 +132,8 @@ public class ThriftFormatter {
             if (coma) {
                 sb.append(", ");
             }
-            sb.append(String.format("%d:%s %s %s", f.getThriftFieldId(), formatTypeName(fmd.valueMetaData),
+            sb.append(String.format("<span class=\"pl-e\">%d:</span><span class=\"pl-k\">%s %s</span> <span class=\"pl-smi\">%s</span>",
+                                    f.getThriftFieldId(), formatTypeName(fmd.valueMetaData, false),
                                     formatRequirementType(fmd.requirementType), fmd.fieldName));
             coma = true;
         }
@@ -109,7 +142,7 @@ public class ThriftFormatter {
         final List<TFieldIdEnum> resultFields = sortedKeys(resultMap.second);
 
         if (resultFields.size() > 1) {
-            sb.append(" throws(");
+            sb.append(" <span class=\"pl-k\">throws</span>(");
             coma = false;
             for (TFieldIdEnum f : resultFields) {
                 FieldMetaData fmd = resultMap.second.get(f);
@@ -117,7 +150,8 @@ public class ThriftFormatter {
                     if (coma) {
                         sb.append(", ");
                     }
-                    sb.append(String.format("%d:%s %s %s", f.getThriftFieldId(), formatTypeName(fmd.valueMetaData),
+                    sb.append(String.format("<span class=\"pl-e\">%d:</span><span class=\"pl-k\">%s %s</span> <span class=\"pl-smi\">%s</span>",
+                                            f.getThriftFieldId(), formatTypeName(fmd.valueMetaData, false),
                                             formatRequirementType(fmd.requirementType), fmd.fieldName));
                     coma = true;
                 }
@@ -126,13 +160,27 @@ public class ThriftFormatter {
             sb.append(")");
         }
 
-        sb.append("</span>\n");
-
+        sb.append("\n");
         return sb.toString();
     }
 
-    public String formatMethod(ThriftControllerInfo cInfo) {
-        return formatMethod(cInfo.getMethodName(), cInfo.getArgCls(), cInfo.getResultCls());
+    public String head() {
+        return "<head>\n" +
+            "  <meta charset=\"utf-8\">\n" +
+            style() +
+            " </head>";
+    }
+
+    public String style() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("<style>\n");
+        sb.append(".pl-e, .pl-en {color: #6f42c1;}\n");
+        sb.append(".pl-k, .pl-k a {color: #d73a49;}\n");
+        sb.append(".pl-c {color: #6a737d;}\n");
+        sb.append(".pl-smi, .pl-s .pl-s1 {color: #24292e;}\n");
+        sb.append(".pl-c1, .pl-s .pl-v {color: #005cc5;}\n");
+        sb.append("</style>\n");
+        return sb.toString();
     }
 
     public String formatClass(Class<? extends TBase> cls) throws ClassNotFoundException {
@@ -151,18 +199,34 @@ public class ThriftFormatter {
 
         final StringBuilder sb = new StringBuilder();
 
+        sb.append(head());
+
         final String type = Exception.class.isAssignableFrom(cls) ? "exception" : "struct";
 
-        sb.append("<div class=\"" + type + "\"><span>" + type + " " + linkClass(_cls.first) + "{</span>\n");
+        sb.append("<div class=\"" + type + "\">\n");
+        sb.append(String.format("<div class=\"pl-c\">%s</div>\n", Optional.ofNullable(cls.getAnnotation(TDoc.class))
+                                                                          .map(TDoc::value)
+                                                                          .orElse("")));
+        sb.append("<br/>");
+        sb.append("<span><span class=\"pl-k\">" + type + "</span> " + linkClass(_cls.first, true) + " {</span>\n");
 
-        sb.append("<ol>\n");
+        sb.append("<table class=\"fields\" style=\"padding-left: 20px;\">\n");
+
         for (TFieldIdEnum f : fields) {
             FieldMetaData fmd = _cls.second.get(f);
-            sb.append(String.format("\t<li style=\"list-style-type: none;\">%d:%s %s %s</li>\n", f.getThriftFieldId(),
-                                    formatTypeName(fmd.valueMetaData), formatRequirementType(fmd.requirementType), fmd.fieldName));
+            sb.append("<tr>\n");
+            sb.append(String.format("<td><span class=\"pl-e\">%d:</span></td>\n", f.getThriftFieldId()));
+            sb.append(String.format("<td><span class=\"pl-k\">%s %s</span></td>\n", formatTypeName(fmd.valueMetaData, true), formatRequirementType(fmd.requirementType)));
+            sb.append(String.format("<td><span class=\"pl-smi\">%s</span></td>\n", fmd.fieldName));
+            sb.append(String.format("<td><span class=\"pl-c\">%s</span></td>\n", Optional.ofNullable(BeanUtils.getPropertyDescriptor(_cls.first, fmd.fieldName)
+                                                                                                              .getReadMethod()
+                                                                                                              .getAnnotation(TDoc.class))
+                                                                                         .map(TDoc::value)
+                                                                                         .orElse("")));
+            sb.append("</tr>\n");
         }
 
-        sb.append("</ol>\n");
+        sb.append("</table>\n");
 
         sb.append("<span>}</span></div>\n");
         return sb.toString();
@@ -178,25 +242,56 @@ public class ThriftFormatter {
         Collections.sort(values, (o1, o2) -> (Ints.compare(o1.getValue(), o2.getValue())));
 
         final StringBuilder sb = new StringBuilder();
+
+        sb.append(head());
+
         sb.append("<div class=\"enum\">\n");
-        sb.append("<span>enum " + cls.getSimpleName() + "{</span>\n");
-        sb.append("<ol>\n");
+        sb.append(String.format("<div class=\"pl-c\">%s</div>\n", Optional.ofNullable(cls.getAnnotation(TDoc.class))
+                                                                          .map(TDoc::value)
+                                                                          .orElse("")));
+        sb.append("<br/>");
+
+        sb.append("<span><span class=\"pl-k\">enum</span> " + linkEnum(cls, true) + " {</span>\n");
+
+        sb.append("<table class=\"fields\" style=\"padding-left: 20px;\">\n");
+
         for (TEnum e : values) {
-            sb.append(String.format("<li style=\"list-style-type: none;\">%s = %d</li>\n", ((Enum) e).name(), e.getValue()));
+            sb.append("<tr>\n");
+            sb.append(String.format("<td><span class=\"pl-smi\">%s</span></td>\n", ((Enum) e).name()));
+            sb.append(String.format("<td><span class=\"pl-c1\"> = %d</span></td>\n", e.getValue()));
+            try {
+                sb.append(String.format("<td style=\"padding-left: 10px;\"><span class=\"pl-c\">%s</span></td>\n", Optional
+                    .ofNullable(cls.getField(((Enum) e).name()).getAnnotation(TDoc.class))
+                    .map(TDoc::value)
+                    .orElse("")));
+            } catch (NoSuchFieldException e1) {
+            }
+            sb.append("</tr>\n");
         }
-        sb.append("</ol>\n");
+
+        sb.append("</table>\n");
         sb.append("<span>}</span></div>\n");
         return sb.toString();
     }
 
-    private String linkClass(Class cls) {
-        return String.format("<a href=\"%s\">%s</a>", basePath + "struct/" + URLEncoder.encode(cls.getCanonicalName()) + "/",
-                             cls.getSimpleName());
+    private String linkClass(Class cls, boolean span) {
+        final String format;
+        if (span) {
+            format = "<a href=\"%s\"><span class=\"pl-en\">%s</span></a>";
+        } else {
+            format = "<a href=\"%s\">%s</a>";
+        }
+        return String.format(format, basePath + "struct/" + URLEncoder.encode(cls.getCanonicalName()) + "/", cls.getSimpleName());
     }
 
-    private String linkEnum(Class cls) {
-        return String.format("<a href=\"%s\">%s</a>", basePath + "enum/" + URLEncoder.encode(cls.getCanonicalName()) + "/",
-                             cls.getSimpleName());
+    private String linkEnum(Class cls, boolean span) {
+        final String format;
+        if (span) {
+            format = "<a href=\"%s\"><span class=\"pl-en\">%s</span></a>";
+        } else {
+            format = "<a href=\"%s\">%s</a>";
+        }
+        return String.format(format, basePath + "enum/" + URLEncoder.encode(cls.getCanonicalName()) + "/", cls.getSimpleName());
     }
 
     public static String formatRequirementType(int requirementType) {
@@ -211,31 +306,31 @@ public class ThriftFormatter {
         return "";
     }
 
-    public String formatTypeName(FieldValueMetaData vmd) {
+    public String formatTypeName(FieldValueMetaData vmd, boolean span) {
 
         if (vmd == null) {
             return "void";
         }
 
         if (vmd instanceof EnumMetaData) {
-            return linkEnum(((EnumMetaData) vmd).enumClass);
+            return linkEnum(((EnumMetaData) vmd).enumClass, span);
         }
 
         if (vmd instanceof ListMetaData) {
-            return "list&lt;" + formatTypeName(((ListMetaData) vmd).elemMetaData) + "&gt;";
+            return "list&lt;" + formatTypeName(((ListMetaData) vmd).elemMetaData, span) + "&gt;";
         }
 
         if (vmd instanceof MapMetaData) {
-            return "map&lt;" + formatTypeName(((MapMetaData) vmd).keyMetaData) + "," + formatTypeName(((MapMetaData) vmd).valueMetaData)
+            return "map&lt;" + formatTypeName(((MapMetaData) vmd).keyMetaData, span) + "," + formatTypeName(((MapMetaData) vmd).valueMetaData, span)
                 + "&gt;";
         }
 
         if (vmd instanceof SetMetaData) {
-            return "list&lt;" + formatTypeName(((SetMetaData) vmd).elemMetaData) + "&gt;";
+            return "list&lt;" + formatTypeName(((SetMetaData) vmd).elemMetaData, span) + "&gt;";
         }
 
         if (vmd instanceof StructMetaData) {
-            return linkClass(((StructMetaData) vmd).structClass);
+            return linkClass(((StructMetaData) vmd).structClass, span);
         }
 
         switch (vmd.type) {
